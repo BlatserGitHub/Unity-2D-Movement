acfa0f069b46a612ccb1437732d5d12a79e39901
Harvey <Harvey>
2022-07-09 17:43:22 +0100

    first commit

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    float moveInput;
+    public float moveSpeed;
+    public float acceleration;
+    public float decceleration;
+    public float velPower;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() 
+    {
+        float targetSpeed = moveInput * moveSpeed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+
+        rb.AddForce(movement * Vector2.right);
+    
+    
+    
+    }
+
+
+
+
+
+}
+
+
